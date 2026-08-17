b4e0520d6583c2a8c6084f993b28d7e01e6ec2e3
Odran <Odran@odran.eu>
2026-08-18 00:40:55 +0200

    Prepare Android companion 0.20.3

--- android/companion/app/build.gradle.kts
+++ android/companion/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "eu.odran.archipelago"
         minSdk = 26
         targetSdk = 35
-        versionCode = 59
-        versionName = "0.20.2"
+        versionCode = 60
+        versionName = "0.20.3"
 
         ndk {
             // The custom mGBA bridge currently targets 64-bit Android devices.
